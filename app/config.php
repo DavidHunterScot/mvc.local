23c85f9c8f32fe537eca4001ee4e096140fe66b7
David Hunter <DavidHunterScot@users.noreply.github.com>
2017-03-12 21:30:35 +0000

    Added siteRootURL variable and its getter to config
    At some point, we may wish to generate a full URL to a certain route,
    path, or file. With this new configuration entry, we can do just that.
    
    For now it will stay in the config.php file, but later might get moved
    to the database.

--- app/config.php
+++ app/config.php
@@ -2,10 +2,16 @@
 
 class Config {
 
+	// Full site root URL where the site can be found,
+	// including protocol, and without trailing slash.
+	private $siteRootURL = "http://mvc.local";
+
 	private $dbHost = "localhost";
 	private $dbUser = "mvc";
 	private $dbPass = "fzaqNJdX2NUS6TKW";
 	private $dbName = "mvc";
+
+	public function getSiteRootURL() { return $this->siteRootURL; }
 
 	public function getDbHost() { return $this->dbHost; }
 	public function getDbUser() { return $this->dbUser; }
